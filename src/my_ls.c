61e0eb919e78d9741b051c3f95842ce01a92e9eb
yanis <yanis.kernoua@epitech.eu>
2023-11-04 19:06:44 +0100

    feat(src): getting options string and handling errors

--- src/my_ls.c
+++ src/my_ls.c
@@ -5,7 +5,18 @@
 ** Main function for my_ls
 */
 
-int main(void)
+#include "Core/options.h"
+#include <stddef.h>
+
+int main(int ac, char **av)
 {
+    char *options_string = NULL;
+    int options_string_index = get_options_string_index(ac, av);
+
+    if (options_string_index != NO_OPTIONS) {
+        options_string = av[options_string_index];
+        if (is_options_string_valid(options_string) != 1)
+            return 84;
+    }
     return 0;
 }
