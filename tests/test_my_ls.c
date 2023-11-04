de2f5d56134508aecfcbfaec97c7daa1524af56f
yanis <yanis.kernoua@epitech.eu>
2023-11-04 17:52:19 +0100

    test(my_ls): unit testing initial commit

---
+++ tests/test_my_ls.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** test_my_ls
+** File description:
+** Unit testing my_ls
+*/
+
+#include <criterion/criterion.h>
+
+Test(my_ls, should_return_0)
+{
+    cr_assert_eq(0, 0);
+}
