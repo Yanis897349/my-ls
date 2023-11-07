3efcfea0adcf322be522cdd0c4e806f4e3eb1234
yanis <yanis.kernoua@epitech.eu>
2023-11-07 22:28:44 +0100

    feat(Core): basic display behavior

--- src/Core/display.c
+++ src/Core/display.c
@@ -16,11 +16,13 @@ void basic_display(directory_t **directories)
     struct dirent *dirent = NULL;
 
     for (int i = 0; directories[i] != NULL; i++) {
-        while ((dirent = readdir(directories[i]->stream)) != NULL) {
+        dirent = readdir(directories[i]->stream);
+        while (dirent != NULL) {
             if (dirent->d_name[0] != '.') {
                 write(1, dirent->d_name, my_strlen(dirent->d_name));
                 write(1, "  ", 2);
             }
+            dirent = readdir(directories[i]->stream);
         }
     }
     write(1, "\n", 1);
