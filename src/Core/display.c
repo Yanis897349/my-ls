0a1bcea2e6d7902e8801ec2af87bf849fbe585fe
yanis <yanis.kernoua@epitech.eu>
2023-11-07 22:28:17 +0100

    feat(Core): basic display behavior

---
+++ src/Core/display.c
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2023
+** display
+** File description:
+** Basic display behavior of my_ls
+*/
+
+#include "directories.h"
+#include "include/my_strings.h"
+#include <stddef.h>
+#include <unistd.h>
+#include <dirent.h>
+
+void basic_display(directory_t **directories)
+{
+    struct dirent *dirent = NULL;
+
+    for (int i = 0; directories[i] != NULL; i++) {
+        while ((dirent = readdir(directories[i]->stream)) != NULL) {
+            if (dirent->d_name[0] != '.') {
+                write(1, dirent->d_name, my_strlen(dirent->d_name));
+                write(1, "  ", 2);
+            }
+        }
+    }
+    write(1, "\n", 1);
+}
---
+++ src/Core/display.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2023
+** display
+** File description:
+** Basic display behavior of my_ls
+*/
+
+#ifndef DISPLAY_H_
+    #define DISPLAY_H_
+    #include "directories.h"
+
+/* FUNCTION PROTOTYPE */
+void basic_display(directory_t **directories);
+
+#endif /* !DISPLAY_H_ */
