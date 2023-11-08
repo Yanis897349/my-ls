8f557d62fedd30a470ff8aa1419f0639a1713d27
yanis <yanis.kernoua@epitech.eu>
2023-11-08 18:06:46 +0100

    tests(files): 🧪 getting files list with correct stat

--- tests/test_display.c
+++ tests/test_display.c
@@ -5,9 +5,9 @@
 ** Unit testing display from my_ls
 */
 
-#include "src/Core/directories.h"
 #include "src/Core/files.h"
 #include "src/Core/display.h"
+#include "src/Helpers/args.h"
 #include <criterion/criterion.h>
 #include <criterion/redirect.h>
 #include <stdlib.h>
@@ -18,15 +18,15 @@ static void redirect_all_std(void)
     cr_redirect_stderr();
 }
 
-Test(display, basic_display, .init = redirect_all_std, .disabled = 1)
+Test(display, basic_display, .init = redirect_all_std)
 {
     int ac = 2;
     char *av[] = {"./my_ls", "tests/test_purposes"};
-    char **files_path = get_files(ac, av);
-    directory_t **directories = open_directories(files_path);
+    char **files_path = get_files_from_args(ac, av);
+    file_t **files_list = get_files_list(files_path);
 
-    basic_display(directories);
+    basic_display(files_list);
     cr_assert_stdout_eq_str("file  test1\n");
-    free_directories(directories);
+    free_files_list(files_list);
     free(files_path);
 }
