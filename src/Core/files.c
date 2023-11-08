d352241bb82575c5e2724f26cddb386f01660330
yanis <yanis.kernoua@epitech.eu>
2023-11-08 18:03:59 +0100

    feat(Core): ✨ files/directory handling with stat

--- src/Core/files.c
+++ src/Core/files.c
@@ -1,58 +1,84 @@
 /*
 ** EPITECH PROJECT, 2023
-** get_files
+** my_ls
 ** File description:
-** Get my_ls files
+** Manage files for my ls
 */
 
-#include "include/my_strings.h"
+#include <sys/types.h>
+#include <dirent.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "include/my_strings.h"
+#include "files.h"
 
-static int get_files_count(int ac, char *const *const av)
+static void print_invalid_file(char *filepath)
 {
-    int files_count = 0;
+    write(2, "my_ls: cannot access '", 22);
+    write(2, filepath, my_strlen(filepath));
+    write(2, "': ", 3);
+    write(2, strerror(errno), my_strlen(strerror(errno)));
+    write(2, "\n", 1);
+}
 
-    for (int i = 1; i < ac; i++) {
-        if (av[i][0] != '-') {
-            files_count += my_strlen(av[i]);
+static int get_valid_files_count(char **files_path)
+{
+    struct stat file_stat;
+    int valid_files_count = 0;
+
+    for (int i = 0; files_path[i] != NULL; i++) {
+        if (stat(files_path[i], &file_stat) != -1) {
+            valid_files_count++;
+        } else {
+            print_invalid_file(files_path[i]);
         }
     }
-    return files_count;
+    return valid_files_count;
 }
 
-static char **no_files_case(void)
+static int set_file(char *filepath, file_t **file)
 {
-    char **files_path = malloc(sizeof(char *) * 2);
-
-    if (files_path == NULL)
-        return NULL;
-    files_path[0] = malloc(2);
-    if (files_path[0] == NULL)
-        return NULL;
-    files_path[0] = ".";
-    files_path[1] = NULL;
-    return files_path;
+    *file = malloc(sizeof(file_t));
+    if (*file == NULL)
+        return 84;
+    (*file)->path = filepath;
+    (*file)->stat = malloc(sizeof(struct stat));
+    if ((*file)->stat == NULL)
+        return 84;
+    if (stat(filepath, (*file)->stat) == -1)
+        return 84;
+    if (S_ISDIR((*file)->stat->st_mode))
+        (*file)->is_directory = 1;
+    else
+        (*file)->is_directory = 0;
+    return 0;
 }
 
-char **get_files(int ac, char *const *const av)
+void free_files_list(file_t **files_list)
 {
-    char **files_path = NULL;
-    int files_count = get_files_count(ac, av);
+    for (int i = 0; files_list[i] != NULL; i++) {
+        free(files_list[i]->stat);
+        free(files_list[i]);
+    }
+    free(files_list);
+}
+
+file_t **get_files_list(char **files_path)
+{
+    int files_count = get_valid_files_count(files_path);
     int files_index = 0;
+    file_t **files_list = malloc(sizeof(file_t *) * (files_count + 1));
 
-    if (files_count == 0) {
-        files_path = no_files_case();
-        return files_path;
-    }
-    files_path = malloc(sizeof(char *) * (files_count + 1));
-    if (files_path == NULL)
+    if (files_list == NULL)
         return NULL;
-    for (int i = 1; i < ac; i++) {
-        if (av[i][0] != '-') {
-            files_path[files_index++] = av[i];
-        }
+    my_memset(files_list, 0, sizeof(file_t *) * (files_count + 1));
+    for (int i = 0; files_path[i] != NULL; i++) {
+        if (set_file(files_path[i], &files_list[files_index++]) == 84)
+            files_index--;
     }
-    files_path[files_count] = NULL;
-    return files_path;
+    files_list[files_count] = NULL;
+    return files_list;
 }
