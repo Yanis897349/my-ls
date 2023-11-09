713fa3ae8f97824d8d23ad0ed8de917827891e7c
yanis <yanis.kernoua@epitech.eu>
2023-11-09 19:09:30 +0100

    feat(Core): ✨ error handling module

---
+++ src/Core/display_error.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** my_ls
+** File description:
+** Display error
+*/
+
+#ifndef DISPLAY_ERROR_H_
+    #define DISPLAY_ERROR_H_
+
+void print_invalid_file(char *filepath);
+
+#endif /* !DISPLAY_ERROR_H_ */
