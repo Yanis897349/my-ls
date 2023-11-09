143f7c83790c0059c2cf87ce43cc704ca7720bd6
yanis <yanis.kernoua@epitech.eu>
2023-11-09 19:11:04 +0100

    feat(Helpers): ✨ sort entries alphabetically

---
+++ src/Helpers/sort.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2023
+** my_ls
+** File description:
+** Sort entries alphabetically
+*/
+
+#ifndef SORT_H_
+    #define SORT_H_
+    #include "Core/files.h"
+
+void sort_alphabetically(file_t **files_list);
+
+#endif /* !SORT_H_ */
