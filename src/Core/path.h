00fc8966cbd4601fc3de6b10e2415a5672ca1eab
yanis <yanis.kernoua@epitech.eu>
2023-11-09 19:10:42 +0100

    feat(Core): ✨ path module to handle fullpath

---
+++ src/Core/path.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** my_ls
+** File description:
+** Path manager
+*/
+
+#ifndef PATH_H_
+    #define PATH_H_
+
+char *extract_name_from_path(char *path);
+
+#endif /* !PATH_H_ */
