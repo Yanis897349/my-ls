273f1217aacd4688912f7d7cdac590f4c9fc2d3e
yanis <yanis.kernoua@epitech.eu>
2023-11-10 14:30:25 +0100

    test: ✅ hidden files with -all flag

--- tests/test_options.c
+++ tests/test_options.c
@@ -20,51 +20,54 @@ Test(options, options_string_set_max)
 {
     int ac = 2;
     char *av[] = {"./my_ls", "-alRdrt"};
-    char *options_string = get_options(ac, av);
+    options_t *options = create_options(ac, av);
 
-    cr_assert_str_eq(options_string, "alRdrt");
-    free(options_string);
+    cr_assert_eq(options->is_all, 1);
+    cr_assert_eq(options->is_long, 1);
+    cr_assert_eq(options->is_recursive, 0);
+    cr_assert_eq(options->is_dir_only, 1);
+    cr_assert_eq(options->is_reverse_sort, 1);
+    cr_assert_eq(options->is_time_sort, 1);
+    free(options);
 }
 
 Test(options, options_string_set_reduced)
 {
     int ac = 2;
     char *av[] = {"./my_ls", "-aRd"};
-    char *options_string = get_options(ac, av);
+    options_t *options = create_options(ac, av);
 
-    cr_assert_str_eq(options_string, "aRd");
-    free(options_string);
+    cr_assert_eq(options->is_all, 1);
+    cr_assert_eq(options->is_long, 0);
+    cr_assert_eq(options->is_recursive, 0);
+    cr_assert_eq(options->is_dir_only, 1);
+    cr_assert_eq(options->is_reverse_sort, 0);
+    cr_assert_eq(options->is_time_sort, 0);
+    free(options);
 }
 
 Test(options, options_string_set_empty)
 {
     int ac = 2;
     char *av[] = {"./my_ls", "-"};
-    char *options_string = get_options(ac, av);
+    options_t *options = create_options(ac, av);
 
-    cr_assert_str_eq(options_string, "");
-    free(options_string);
-}
-
-Test(options, options_string_with_supported_flags)
-{
-    int ac = 2;
-    char *av[] = {"./my_ls", "-aRdrt"};
-    char *options_string = get_options(ac, av);
-    int is_valid = is_options_string_valid(options_string);
-
-    cr_assert_eq(is_valid, 1);
-    free(options_string);
+    cr_assert_eq(options->is_all, 0);
+    cr_assert_eq(options->is_long, 0);
+    cr_assert_eq(options->is_recursive, 0);
+    cr_assert_eq(options->is_dir_only, 0);
+    cr_assert_eq(options->is_reverse_sort, 0);
+    cr_assert_eq(options->is_time_sort, 0);
+    free(options);
 }
 
 Test(options, options_string_with_not_supported_flag, .init = redirect_all_std)
 {
     int ac = 2;
     char *av[] = {"./my_ls", "-aRdX"};
-    char *options_string = get_options(ac, av);
-    int is_valid = is_options_string_valid(options_string);
+    options_t *options = create_options(ac, av);
 
-    cr_assert_eq(is_valid, 84);
+    cr_assert_eq(options, NULL);
     cr_assert_stderr_eq_str("my_ls: invalid option -- 'X'\n");
-    free(options_string);
+    free(options);
 }
