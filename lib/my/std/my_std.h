24308b7490a71057778eddf34faaf8bc848b0ea4
yanis <yanis.kernoua@epitech.eu>
2023-11-14 14:46:01 +0100

    feat(lib): ✨ stdlib implementation (realloc)

---
+++ lib/my/std/my_std.h
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2023
+** stdlib
+** File description:
+** Implementatiof of stdlib
+*/
+
+#pragma once
+#include <stddef.h>
+
+void *my_realloc(void *ptr, size_t old_size, size_t new_size);
